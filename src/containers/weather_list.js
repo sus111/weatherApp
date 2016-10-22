90eb81c86e4e80be22df2ee659af37987e4750ea
sus111 <sgraham535@gmail.com>
2016-10-22 15:50:19 +0100

    add map

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component {
 
   renderWeather(cityData){
     const name = cityData.city.name;
-    const temps = cityData.list.map(weather => weather.main.temp)
-    const pressures = cityData.list.map(weather => weather.main.pressure)
-    const humidities = cityData.list.map(weather => weather.main.humidity)
-
+    const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = cityData.city.coord;
 
     return(
       <tr key={name}>
-        <td>{name}</td>
-        <td><Chart data={temps} color={"red"}/></td>
-        <td><Chart data={pressures} color={"green"}/></td>
-        <td><Chart data={humidities} color={"orange"}/></td>
+        <td><GoogleMap lon={lon} lat={lat}/></td>
+        <td><Chart data={temps} color={"red"} units="C"/></td>
+        <td><Chart data={pressures} color={"green"} units="hPa"/></td>
+        <td><Chart data={humidities} color={"orange"} units="%"/></td>
       </tr>
     )
   }
